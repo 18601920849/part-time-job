9bff6428c99e94fbbbc0cbc75bd962e8fc5a7927
zhanghai <18601920849@163.com>
2024-11-09 11:28:02 +0800

    first

---
+++ src/main/java/com/entnews/EntnewsApplication.java
@@ -0,0 +1,13 @@
+package com.entnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntnewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntnewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entnews/EntnewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntnewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
